f921c859d1b1708666b7d9cddd8afa31a5d36abf
Mirko Teschke <mirko.teschke@gmail.com>
2026-06-18 14:19:46 -0700

    Simplify Color Change For Now

--- Theories.py
+++ Theories.py
@@ -2,10 +2,11 @@ from Transformations import (
     Theory,
     rotate_90, rotate_180, rotate_270,
     mirror_horizontally, swap_colors, make_hollow,
-    crop_to_content, recolor, make_color_map_fn,
+    crop_to_content, recolor,
 )
 from Observations import Observations
 
+ARC_COLORS = range(10)
 
 SAME_SIZE_THEORIES: list[Theory] = [
     [rotate_90],
@@ -21,6 +22,13 @@ SIZE_REDUCING_THEORIES: list[Theory] = [
     [crop_to_content, swap_colors],
 ]
 
+RECOLOR_THEORIES: list[Theory] = [
+    [recolor(fc, tc)]
+    for fc in ARC_COLORS
+    for tc in ARC_COLORS
+    if fc != tc
+]
+
 
 def get_theories(obs: Observations) -> list[Theory]:
     theories: list[Theory] = []
@@ -31,12 +39,6 @@ def get_theories(obs: Observations) -> list[Theory]:
     if obs.size_decreases:
         theories.extend(SIZE_REDUCING_THEORIES)
 
-    if obs.color_map:
-        theories.append([make_color_map_fn(obs.color_map)])
-
-    for fc, tc in obs.recolor_pairs:
-        theories.append([recolor(fc, tc)])
-        if obs.same_size:
-            theories.append([swap_colors, recolor(fc, tc)])
+    theories.extend(RECOLOR_THEORIES)
 
     return theories
